1d6797e83b26b7f0c1834b1a169935ed30799ebe
Faiq Malik <faiqmalik2@gmail.com>
2022-02-01 14:45:35 -0500

    updated packages

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,12 @@ app.get('/', (req: any, res: any) => {
     res.send('Hello World!')
 })
 
+app.get('/derp', (req: any, res: any) => {
+    console.log("DERP Called");
+
+    res.json({ msg: 'derp' })
+})
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
